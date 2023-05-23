19a321514f348f9bece05422a625324a0a798fc7
AugmentedDuck <anders.m.matzen@gmail.com>
2023-05-23 23:08:25 +0200

    Started on correctly guessed letters

--- src/hangmanExectutioner.js
+++ src/hangmanExectutioner.js
@@ -3,22 +3,27 @@ import userWords from "./userWords.js"
 
 var form = document.getElementById("wordLength");
 var word = ""
-var unknownWord = "The Word:"
+var unknownWord = ""
 var guessedLetters = ""
 var wrongLetters = ""
 var nextLetter = ""
 var knownPositions = ""
 const possibleWords = words.concat(userWords)
-
+const unknownButtons = []
 
 function HandleForm(event) {
   event.preventDefault();
   var unknownWordText = document.getElementById("unknownWordText")
   var wordSize = document.getElementById("size").value;
+  var idIndex = 0
+  console.log(unknownWordText)
 
   for (let i = 0; i < wordSize; i++) {
-    unknownWord += " _"
+    unknownWord += `<button id="button_${idIndex}">?</button>`
+    
+    //unknownButtons[idIndex].addEventListener('click', WrongLetter)
     knownPositions += "?"
+    idIndex++
   }
 
   for (let i = possibleWords.length - 1; i >= 0; i--) {
@@ -29,13 +34,21 @@ function HandleForm(event) {
 
   console.log(possibleWords)
 
-  unknownWordText.innerHTML = `<p>${unknownWord}</p>`
+  unknownWordText.innerHTML = unknownWord
 
+  for (let i = 0; i < wordSize; i++) {
+    unknownButtons.push(document.getElementById(`button_${idIndex}`))
+    console.log(unknownButtons)
+  }
   NextGuess();
 }
 
 form.addEventListener('submit', HandleForm);
 document.getElementById('noButton').addEventListener('click', WrongLetter)
+
+for(let i = 0; i < unknownButtons.length; i++) {
+  
+}
 
 function NextGuess() {
   let letters = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
@@ -53,16 +66,26 @@ function NextGuess() {
       }
     }
 
-    let indexOfNextLetter = IndexOfMax(letters);
-    console.log(letters)
+    dataSorter(letters);
+  }
+
+  function dataSorter(arr) {
+    let indexOfNextLetter = IndexOfMax(arr);
+    console.log(arr)
     nextLetter = String.fromCharCode(97 + indexOfNextLetter);
+    for(let i = 0; i < guessedLetters.length; i++){
+      if (guessedLetters[i] == nextLetter) {
+        arr[indexOfNextLetter] = 0
+        dataSorter(arr)
+      }
+    }
   }
 
   let nextGuessText = document.getElementById("guess")
   nextGuessText.innerHTML = `<p>Is there an "${nextLetter}" ?`
 
   let usedLetterText = document.getElementById("usedLettersText")
-  usedLetterText.innerHTML = `<p>Used Letter: "${wrongLetters}" ?`
+  usedLetterText.innerHTML = `<p>Used Letter: "${wrongLetters}"`
 }
 
 function IndexOfMax(arr) {
@@ -79,7 +102,8 @@ function IndexOfMax(arr) {
   return maxIndex
 }
 
-function CorrectLetter(){
+function CorrectLetter(index){
+  knownPositions[index] = nextLetter
   guessedLetters += nextLetter
 }
 
@@ -102,11 +126,10 @@ function UpdateWordList() {
         }
       }
 
-      for (let l = 0; l < wrongLetters.length; l++) {
-        if(wrongLetters[l] == possibleWords[i][j]) {
-          spliceIndex[index] = i;
-        }
+      if(wrongLetters[wrongLetters.length - 1] == possibleWords[i][j]) {
+        spliceIndex[index] = i;
       }
+
     }
     if(spliceIndex[index]) {
       index++
